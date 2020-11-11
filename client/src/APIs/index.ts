d97171a90f19a6bc12fbcdea5625fc594c0edfe5
pradeep-selva <pradeepselvaraju@gmail.com>
2020-11-12 03:49:13 +0530

    User apis setup

--- client/src/APIs/index.ts
+++ client/src/APIs/index.ts
@@ -1,2 +1,4 @@
 export * from "./axios";
 export * from "./auth";
+export * from "./feed";
+export * from "./user";
---
+++ client/src/APIs/user.ts
@@ -0,0 +1,60 @@
+import { ENDPOINTS } from "../Configs";
+import { retrieveResponse } from "../Services/api";
+import { RawResponse } from "../Types";
+import axios from "./axios";
+
+export const getUserData = () =>
+  axios
+    .getInstance()
+    .get<RawResponse>(ENDPOINTS.user)
+    .then((response) => retrieveResponse(response.data));
+
+export const updateUserData = (payload: FormData) =>
+  axios
+    .getInstance()
+    .put<RawResponse>(ENDPOINTS.user, payload, {
+      headers: { "Content-Type": `multipart/form-data` }
+    })
+    .then((response) => retrieveResponse(response.data));
+
+export const deactivateUser = () =>
+  axios
+    .getInstance()
+    .delete<RawResponse>(ENDPOINTS.user)
+    .then((response) => retrieveResponse(response.data));
+
+export const getUserById = (id: string) =>
+  axios
+    .getInstance()
+    .get<RawResponse>(ENDPOINTS.userById(id))
+    .then((response) => retrieveResponse(response.data));
+
+export const getUserPosts = (id: string) =>
+  axios
+    .getInstance()
+    .get<RawResponse>(ENDPOINTS.publicUserPosts(id))
+    .then((response) => retrieveResponse(response.data));
+
+export const getUserUpvotes = () =>
+  axios
+    .getInstance()
+    .get<RawResponse>(ENDPOINTS.userUpvotes)
+    .then((response) => retrieveResponse(response.data));
+
+export const getUserDownvotes = () =>
+  axios
+    .getInstance()
+    .get<RawResponse>(ENDPOINTS.userDownvotes)
+    .then((response) => retrieveResponse(response.data));
+
+export const getUserNotifications = () =>
+  axios
+    .getInstance()
+    .get<RawResponse>(ENDPOINTS.userNotifications)
+    .then((response) => retrieveResponse(response.data));
+
+export const setUserNotificationsSeen = () =>
+  axios
+    .getInstance()
+    .post<RawResponse>(ENDPOINTS.userNotifications)
+    .then((response) => retrieveResponse(response.data));
