9497136cc008a6fe4b31a702d32a176073ae581d
pradeep-selva <pradeepselvaraju@gmail.com>
2020-12-11 13:27:00 +0530

    Pagination in subreaddit page

--- client/src/Pages/Subreaddit/container.tsx
+++ client/src/Pages/Subreaddit/container.tsx
@@ -8,7 +8,7 @@ import {
 } from "@material-ui/core";
 import React, { Component } from "react";
 import { getSubPosts } from "../../APIs";
-import { PostCard } from "../../Components";
+import { PaginationContainer, PostCard } from "../../Components";
 import { STATUS_SUCCESS } from "../../Configs";
 import { IPost } from "../../Types";
 import { IProps as ReduxProps } from "./index";
@@ -25,6 +25,7 @@ interface SelfProps {
 interface IState {
   offset: number;
   posts: Array<IPost>;
+  hasMoreToFetch: boolean;
 }
 
 type IProps = SelfProps & IClass & ReduxProps;
@@ -36,12 +37,13 @@ class Subreaddit extends Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
 
-    this.limit = 15;
+    this.limit = 5;
     this.sub = this.props.match?.params?.id || "";
 
     this.state = {
       offset: 0,
-      posts: []
+      posts: [],
+      hasMoreToFetch: true
     };
   }
 
@@ -49,35 +51,63 @@ class Subreaddit extends Component<IProps, IState> {
     getSubPosts(this.sub, this.state.offset, this.limit).then(
       ({ data, statusCode }) => {
         if (statusCode === STATUS_SUCCESS) {
-          this.setState({ posts: data as Array<IPost> });
+          this.setState({ posts: data as Array<IPost> }, () => {
+            if (data?.length < this.limit) {
+              this.setState({ hasMoreToFetch: false });
+            }
+          });
         }
       }
     );
   }
+
+  fetchMore = () => {
+    if (this.state.hasMoreToFetch) {
+      getSubPosts(this.sub, this.state.offset + this.limit).then(
+        ({ data, statusCode }) => {
+          if (statusCode === STATUS_SUCCESS) {
+            this.setState(
+              (state) => ({
+                posts: [...state.posts, ...data],
+                offset: state.offset + this.limit
+              }),
+              () => {
+                if (data?.length < this.limit) {
+                  this.setState((state) => ({ hasMoreToFetch: false }));
+                }
+              }
+            );
+          }
+        }
+      );
+    }
+  };
 
   render() {
     const { posts } = this.state;
     const { classes } = this.props;
 
     return (
-      <Container>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={8}>
-            <Typography color={"textPrimary"} className={classes.postsTitle}>
-              Posts from {this.sub}
-              <Box style={{ marginTop: "1.5rem" }}>
-                {!!posts.length &&
-                  posts.map((item, index) => (
-                    <PostCard {...item} key={index} />
-                  ))}
-              </Box>
-            </Typography>
+      <PaginationContainer handlePagination={this.fetchMore}>
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={8}>
+              <Typography color={"textPrimary"} className={classes.postsTitle}>
+                Posts from {this.sub}
+                <Box style={{ marginTop: "1.5rem" }}>
+                  {!!posts.length &&
+                    posts.map((item, index) => (
+                      <PostCard {...item} key={index} />
+                    ))}
+                </Box>
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Paper></Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </PaginationContainer>
     );
   }
 }
