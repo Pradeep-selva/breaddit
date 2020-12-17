c1c1d78c1792e7f632fc81b990d3b136b444da1f
pradeep-selva <pradeepselvaraju@gmail.com>
2020-12-17 17:19:15 +0530

    fixing redux casing issue

--- client/src/Redux/Saga/index.ts
+++
@@ -1,8 +0,0 @@
-import { all, fork } from "redux-saga/effects";
-import feedSaga from "./feed";
-import userSaga from "./user";
-
-export default function* Saga() {
-  yield all([fork(feedSaga)]);
-  yield all([fork(userSaga)]);
-}
--- client/src/Redux/reducers/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as feedReducer } from "./feed";
-export { default as userReducer } from "./user";
