129dc8bb81a6bad02b1bba1ce1a5e4b9da74f2a0
pradeep-selva <pradeepselvaraju@gmail.com>
2021-01-06 00:15:01 +0530

    Minor ui change -- wave animation on skeletons

--- client/src/Components/Skeletons/PostSkeleton.tsx
+++ client/src/Components/Skeletons/PostSkeleton.tsx
@@ -20,29 +20,49 @@ const PostSkeleton = ({ height }: IProps) => {
             height={30}
             style={{ marginTop: "1rem" }}
           />
-          <Skeleton variant={"circle"} width={10} height={10} />
-          <Skeleton variant={"circle"} width={30} height={30} />
+          <Skeleton
+            variant={"circle"}
+            width={10}
+            height={10}
+            animation={"wave"}
+          />
+          <Skeleton
+            variant={"circle"}
+            width={30}
+            height={30}
+            animation={"wave"}
+          />
         </Box>
         <Box className={classes.contentSection}>
           <Box className={classes.titleSection} style={{ flex: 1 }}>
-            <Skeleton width={"75%"} />
+            <Skeleton width={"75%"} animation={"wave"} />
           </Box>
           <Box className={classes.flexContainer} style={{ flex: 5 }}>
             <Box
               className={classes.textDetailsContainer}
               style={{ marginTop: "1%" }}
             >
-              <Skeleton width={"90%"} />
-              <Skeleton width={"80%"} />
-              <Skeleton width={"85%"} />
+              <Skeleton width={"90%"} animation={"wave"} />
+              <Skeleton width={"80%"} animation={"wave"} />
+              <Skeleton width={"85%"} animation={"wave"} />
             </Box>
           </Box>
           <Box style={{ flex: 0.7 }} className={classes.bottomBarContainer}>
             <Box style={{ flex: 1 }} className={classes.bottomBarElement}>
-              <Skeleton variant={"circle"} width={30} height={30} />
+              <Skeleton
+                variant={"circle"}
+                width={30}
+                height={30}
+                animation={"wave"}
+              />
             </Box>
             <Box style={{ flex: 6 }} className={classes.bottomBarElement}>
-              <Skeleton variant={"circle"} width={30} height={30} />
+              <Skeleton
+                variant={"circle"}
+                width={30}
+                height={30}
+                animation={"wave"}
+              />
             </Box>
           </Box>
         </Box>
